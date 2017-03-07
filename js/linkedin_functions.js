c3a5766bdaa5c7c2aa842d9f89592ef2c4bcb3e0
Philip Greenwald <philip@avidest.com>
2017-03-07 18:56:59 -0500

    now imports persons company and title

--- js/linkedin_functions.js
+++ js/linkedin_functions.js
@@ -14,13 +14,25 @@
 
     var capture_data = function(){
         console.log('capturing data');
-        var user_data = IN.API.Raw().url('/people/~:(firstName,email-address,lastName,id,num-connections,picture-url,industry,headline,summary)?format=json').result(put_data_in_form);
+        var user_data = IN.API.Raw().url('/people/~:(firstName,email-address,lastName,id,num-connections,picture-url,industry,headline,summary,positions)?format=json').result(put_data_in_form);
     }
 
     var put_data_in_form = function(data){
         console.log('in put data in form function');
         console.log(data);
         console.log($('#person_first_name'));
+
+
+        // put company and position title into fields
+        $.each(data.positions.values, function( index, value ) {
+            // alert(value.isCurrent);
+            if(value.isCurrent == true){
+                console.log("current position is " + value.title);
+                $('#person_company').val(value.company.name);
+                $('#person_title').val(value.title);
+                return false; //break the loop
+            }
+        });
         $('#person_first_name').val(data.firstName);
         $('#person_last_name').val(data.lastName);
         $('#person_email').val(data.emailAddress);
@@ -28,7 +40,7 @@
         $('#picture_frame').show();
         $('#picture_frame').attr('src', data.pictureUrl);
         $('#person_remote_pic_url').val(data.pictureUrl);
-        $('#person_title').val(data.headline);
+        // $('#person_title').val(data.headline); took this out because now company goes in here
         $('#person_bio').val(data.summary);
         // $('#person_industry').val(data.industry);
         log_out();
