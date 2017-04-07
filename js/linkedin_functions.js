9a3c393edc6f77c08313a59ada28a19cd8a2b028
Philip Greenwald <philip@avidest.com>
2017-04-07 17:13:33 -0400

    linkedin functionality and entrepreneurial experience functionality

--- js/linkedin_functions.js
+++ js/linkedin_functions.js
@@ -14,14 +14,15 @@
 
     var capture_data = function(){
         console.log('capturing data');
-        var user_data = IN.API.Raw().url('/people/~:(firstName,email-address,lastName,id,num-connections,picture-url,industry,headline,summary,positions)?format=json').result(put_data_in_form);
+        var user_data = IN.API.Raw().url('/people/~:(firstName,email-address,lastName,id,num-connections,picture-url,industry,headline,summary,positions,public-profile-url)?format=json').result(put_data_in_form);
     }
 
     var put_data_in_form = function(data){
         console.log('in put data in form function');
         console.log(data);
         console.log($('#person_first_name'));
-
+        console.log('url');
+        console.log(data.publicProfileUrl);
 
         // put company and position title into fields
         $.each(data.positions.values, function( index, value ) {
@@ -42,6 +43,7 @@
         $('#person_remote_pic_url').val(data.pictureUrl);
         // $('#person_title').val(data.headline); took this out because now company goes in here
         $('#person_bio').val(data.summary);
+        $('#linkedin_public_profile_url').val(data.publicProfileUrl);
         // $('#person_industry').val(data.industry);
         log_out();
     }
