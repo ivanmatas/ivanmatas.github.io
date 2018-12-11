3017441e83f0bdd723d6df3506c60666a7b4f490
ivanmatas <ivan.matas@arsfutura.co>
2018-12-11 16:30:24 +0100

    Fix tapstage url

--- js/checkout.js
+++ js/checkout.js
@@ -271,7 +271,7 @@ function createNewPerson(personDataObject) {
 function updatePerson(personDataObject) {
     $.ajax({
         // url: 'http://localhost:3000/people/input_form/' + personId,
-        url: 'http://tapstage.herokuapp.compeople/input_form/' + personId,
+        url: 'https://tapstage.herokuapp.compeople/input_form/' + personId,
         // url: 'http://doorman-backend.herokuapp.compeople/input_form/' + personId,
         type: 'PUT',
         data: personDataObject,
